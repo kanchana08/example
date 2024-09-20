db30ec9094fe0e91eba033dac0b5039b30a63229
kanchana08 <127290267+kanchana08@users.noreply.github.com>
2024-09-20 14:43:36 +0530

    Update hellowWorld.groovy

--- vars/hellowWorld.groovy
+++ vars/hellowWorld.groovy
@@ -1,19 +1,19 @@
  def call() {
 //sh"docker build -t image:latest ."
  // sh "docker images"
-  //sh "pwd"
-  //sh "ls"
-pipeline {
-    agent any
+  sh "pwd"
+  sh "ls"
+//pipeline {
+    //agent any
 
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-            }
-        }
-    }
-}
+    //stages {
+        //stage('Hello') {
+            //steps {
+                //echo 'Hello World'
+            //}
+        //}
+    //}
+//}
 
   
 }
